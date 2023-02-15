94b82189a908fffab773521883104e7440d7fb55
ThomasTr99 <thomas.tran9989@gmail.com>
2023-02-15 04:32:10 -0600

    Added winning_move function

--- connectCMD.py
+++ connectCMD.py
@@ -5,7 +5,7 @@ COL_COUNT = 7
 
 def create_board():
     #Will create a board of zeros with 6 rows and 7 columns and return it
-    board = np.zeros((6,7))
+    board = np.zeros((ROW_COUNT,COL_COUNT))
     return board
 
 def drop_piece(board, row, col, piece):
@@ -26,6 +26,19 @@ def get_next_open_row(board, col):
 def print_board(board):
     print(np.flip(board, 0))
 
+def winning_move(board, piece):
+    #Check horizonal locations for win
+    for c in range(COL_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    #Check vertical locations for win
+
+
+
+
+
 board = create_board()
 print_board(board)
 game_over = False
@@ -39,12 +52,20 @@ while not game_over:
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 1)
+
+            if winning_move(board, 1):
+                print("Player 1 Wins")
+                game_over = True
     #Ask for Player 2 input
     else:
         col = int(input("Player 2: Make Your Selection (0-6): "))
         if is_valid_location(board, col):
             row = get_next_open_row(board, col)
             drop_piece(board, row, col, 2)
+
+            if winning_move(board, 2):
+                print("Player 2 Wins")
+                game_over = True
     print_board(board)
     turn += 1
     #This will allow the turn number to alternate between 0 and 1 for each player
